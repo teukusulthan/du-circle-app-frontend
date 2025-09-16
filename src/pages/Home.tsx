99fa35c76216aafd55e020c9f7df4b9527619eff
teukusulthan <teukusultanul@gmail.com>
2025-09-16 15:23:34 +0700

    feat(home): implement responsive thread list layout and integrate API fetching

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import ThreadList from "../components/ThreadList";
 export default function Home() {
   return (
     <div>
+      {/*Header */}
       <h1 className="my-3 mx-5 font-semibold text-2xl">Home</h1>
       {/* Feed */}
       <ThreadList />
